580e1a7bf7c8fbc60e960a6de1847963dda4adee
orlando seoanes <oaseoanes@unicesar.edu.co>
2023-11-24 21:43:48 -0500

    cambios

--- src/components/Acciones.jsx
+++ src/components/Acciones.jsx
@@ -5,41 +5,35 @@ const StockHistoryChart = () => {
   const [stockData, setStockData] = useState(null);
   const [selectStock, setSelectStock] = useState("TSLA");
   const [formData, setFormData] = useState({
-    Seleccionado: "",
+    Seleccionado: "TSLA", // Set default value to a valid stock code
   });
 
   useEffect(() => {
     const fetchData = async () => {
-      const apiUrl =
-        "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history";
+      const apiUrl = `https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=${formData.Seleccionado}&interval=5m&diffandsplits=false`;
       const apiKey = "3d1831fe92mshea6674bb53d4369p17d7b4jsn2a3626ea0f6f";
 
       try {
         const response = await axios.get(apiUrl, {
-          params: {
-            symbol: formData.Seleccionado,
-            interval: "5m",
-            diffandsplits: "false",
-          },
           headers: {
             "X-RapidAPI-Key": apiKey,
             "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com",
           },
         });
-
+        console.log("API Response:", response.data);
       } catch (error) {
         console.error("Error fetching stock data:", error);
       }
     };
-    console.log(response.data.meta.symbol)
+
     fetchData();
-  }, []);
+  }, [formData.Seleccionado]);
 
   useEffect(() => {
     axios
       .get("https://modelos-2ecbf-default-rtdb.firebaseio.com/EMPRESAS.json")
       .then((response) => {
-        setStockData(response.data);  // Asumo que la estructura de datos es un objeto con propiedades como 'meta' y 'body'
+        setStockData(response.data); // Assuming the data structure is an object with properties like 'meta' and 'body'
       })
       .catch((error) => {
         console.error(error);
@@ -50,7 +44,8 @@ const StockHistoryChart = () => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
-  console.log(formData.Seleccionado)
+
+  console.log(formData.Seleccionado);
 
   return (
     <div>
@@ -62,7 +57,7 @@ const StockHistoryChart = () => {
       >
         {stockData &&
           Object.keys(stockData).map((key) => (
-            <option key={key} value={key}>
+            <option key={key} value={stockData[key]}>
               {key}
             </option>
           ))}
